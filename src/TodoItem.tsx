c683e643189fb0344f1567e7326bd1b3b9921781
matsuhisa <matsuhisa+github@gmail.com>
2021-02-27 12:54:25 +0900

    WIP

--- src/TodoItem.tsx
+++ src/TodoItem.tsx
@@ -7,13 +7,56 @@ type TodoProps = {
   item: Todo
 }
 
+const replaceItemAtIndex = (list: Todo[], index: number, newValue: Todo) => {
+  return [
+    ...list.slice(0, index),
+    newValue,
+    ...list.slice(index+1)
+  ]
+}
+
+const removeItemAtIndex = (list: Todo[], index: number) => {
+  return [
+    ...list.slice(0, index),
+    ...list.slice(index+1)
+  ]
+}
+
 export const TodoItem: FC<TodoProps> = ( {item} ) => {
-  // const [todoList, setTodoList] = useRecoilState(todoListState)
-  // const index = todoList.findIndex( (listItem) => listItem == item )
+  const [todoList, setTodoList] = useRecoilState(todoListState)
+  const index = todoList.findIndex( (listItem) => listItem == item )
+
+  const toggleItemCompletion = () =>{
+    const newList = replaceItemAtIndex(todoList, index, {
+      ...item,
+      isComplete: !item.isComplete,
+    })
+    setTodoList(newList)
+  }
+
+  const editItemText = (event: React.ChangeEvent<HTMLInputElement>) =>{
+    const newList = replaceItemAtIndex(todoList, index, {
+      ...item,
+      text: event.target.value,
+    })
+    setTodoList(newList)
+  }
+
+  const deleteItem = () =>{
+    const newList = removeItemAtIndex(todoList, index)
+    setTodoList(newList)
+  }
 
   return(
     <div>
-      {item.text}
+      <input type="text" value={item.text} onChange={editItemText} />
+      <input
+        type="checkbox"
+        checked={item.isComplete}
+        onChange={toggleItemCompletion}
+      />
+      <button onClick={deleteItem}>削除</button>
+
     </div>
   )
 } 
